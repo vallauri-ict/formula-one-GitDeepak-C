362d02c1322f7b85b5364341b34aa2e1daf9e4cd
GitDeepak-C <d.chopra.0505@vallauri.edu>
2021-01-14 13:18:45 +0100

    Add ToWebApi_Project

---
+++ ToWebApi_Project/Models/toDoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToWebApi_Project.Models
+{
+    public class toDoContext : DbContext
+    {
+        public toDoContext(DbContextOptions<toDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<toDoItem> TodoItems { get; set; }
+    }
+}
